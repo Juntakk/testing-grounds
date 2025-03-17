43c476e41d863caa1325826ea5f2ba3e9dc43990
Nicolas Gauthier <nick.habashi@gmail.com>
2025-03-16 20:05:58 -0400

    Data Table

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { DataTableDemo } from "@/components/DataTable";
 import Layout1 from "@/components/Layout1";
 import PaginationComponent from "@/components/PaginationComponent";
 
 export default function Home() {
   return (
     <div>
+      <DataTableDemo />
       <Layout1 />
       <PaginationComponent />
     </div>
